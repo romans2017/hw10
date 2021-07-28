f4f17ba00defa2121f6758bcde0db7fad0a08e11
romans2017 <savchuk.r0@gmail.com>
2021-07-28 22:27:48 +0300

    Begin random

--- src/main/java/ua/goit/hw10stream/SimpleStream.java
+++ src/main/java/ua/goit/hw10stream/SimpleStream.java
@@ -3,8 +3,11 @@ package ua.goit.hw10stream;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public class SimpleStream {
 
@@ -23,6 +26,16 @@ public class SimpleStream {
 
     public static String sortUpperStrings(List<String> list) {
         return list.stream().map(String::toUpperCase).sorted(Comparator.reverseOrder()).collect(Collectors.joining(" "));
+    }
+
+    public static Stream<Long>generatePseudoRandom(long c, long m, long seed) {
+        UnaryOperator<Long> pseudoRandom = new UnaryOperator<Long>() {
+            @Override
+            public Long apply(Long aLong) {
+                return null;
+            }
+        }
+        return Stream.iterate(seed, )
     }
 
 }
