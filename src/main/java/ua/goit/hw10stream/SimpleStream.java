91938cfcca8dbae8e09f9ce3c289900e47cbdea6
romans2017 <savchuk.r0@gmail.com>
2021-07-30 11:41:39 +0300

    Change generatePseudoRandom - use BigInt and shifting bits to right

--- src/main/java/ua/goit/hw10stream/SimpleStream.java
+++ src/main/java/ua/goit/hw10stream/SimpleStream.java
@@ -1,6 +1,8 @@
 package ua.goit.hw10stream;
 
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,15 +25,38 @@ public class SimpleStream {
     }
 
     public static String sortUpperStrings(List<String> list) {
-        return list.stream().map(String::toUpperCase).sorted(Comparator.reverseOrder()).collect(Collectors.joining(" "));
+        return list.stream()
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining(" "));
     }
 
     public static String getNumbers(String[] numbers) {
-        return Arrays.stream(numbers).filter(item -> item.matches("[0-9]"))..sorted(Comparator.naturalOrder()).collect(Collectors.joining(", "));
+        return Arrays.stream(numbers)
+                .map(item -> item.replaceAll("\\D+", ",")) //get arrays of String numbers with comma as delimiter
+                .flatMap(item -> Arrays.stream(item.split(","))) //get stream with String numbers
+                .mapToInt(Integer::parseInt)//get IntStream to sort as numbers (not strings)
+                .sorted()
+                .mapToObj(String::valueOf)//convert to String for output
+                .collect(Collectors.joining(", "));
     }
 
-    public static Stream<Long>generatePseudoRandom(Long a, Long c, Long m, Long seed) {
-        return Stream.iterate(seed, (Long x) -> (a * x + c) % m);
+    public static Stream<Long> generatePseudoRandom(long a, long c, long m, long seed) {
+        if (a > Long.MAX_VALUE / a) {
+            BigInteger bigA = new BigInteger(String.valueOf(a));
+            BigInteger bigC = new BigInteger(String.valueOf(c));
+            BigInteger bigM = new BigInteger(String.valueOf(m));
+            BigInteger bigSeed = new BigInteger(String.valueOf(seed));
+            return Stream.iterate(bigSeed, (BigInteger x) -> bigA.multiply(x).add(bigC).mod(bigM).shiftRight(16))
+                    .mapToLong(BigInteger::longValue)
+                    .boxed();
+        } else {
+            return Stream.iterate(seed, x -> (a * x + c) % m);
+        }
     }
+
+    /*public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+
+    }*/
 
 }
