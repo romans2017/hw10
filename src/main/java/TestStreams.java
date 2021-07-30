6e5f3121412e3bd6160206616098bce6baa5ad1d
romans2017 <savchuk.r0@gmail.com>
2021-07-30 16:51:25 +0300

    fix random

--- src/main/java/TestStreams.java
+++ src/main/java/TestStreams.java
@@ -10,8 +10,8 @@ public class TestStreams {
         System.out.println(SimpleStream.oddIndexesStrings(list));
         System.out.println(SimpleStream.sortUpperStrings(list));
         System.out.println(SimpleStream.getNumbers(new String[]{"1, 2, 0", "5dgd152ujy45"}));
-        System.out.println(SimpleStream.generatePseudoRandom(25214903917L, 11L, (long) 2e48, 10L).limit(20).collect(Collectors.toList()));
-        System.out.println(SimpleStream.generatePseudoRandom(5, 11L, (long) 2e16, 10).limit(20).collect(Collectors.toList()));
+        System.out.println(SimpleStream.generatePseudoRandom(25214903917L, 11L, 0x1_0000_0000_0000L, 10L).limit(20).collect(Collectors.toList()));
+        System.out.println(SimpleStream.generatePseudoRandom(7L, 7, (long) 10, 7).limit(20).collect(Collectors.toList()));
         System.out.println(SimpleStream.zip(Stream.of(1, 2), Stream.of("A", "B", "C", "D")).collect(Collectors.toList()));
     }
 }
